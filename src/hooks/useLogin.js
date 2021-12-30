bd54e5a1f8e5a97fc046ef3de48300bf0aaa9d5a
App Academy Student <lxg073000@gmail.com>
2021-12-30 09:10:20 -0500

    update online:bool for currentUser doc in db to true

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { auth } from "../firebase.config";
+import { auth, db } from "../firebase.config";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useAuthContext } from "./useAuthContext";
+import { doc, updateDoc } from "firebase/firestore";
 
 export const useLogin = () => {
   const [isCancelled, setIsCancelled] = useState(false);
@@ -18,13 +19,18 @@ export const useLogin = () => {
       const res = await signInWithEmailAndPassword(auth, email, password);
 
       // dispatch login action
-      dispatch({ type: "LOGIN", payload: res.user });
+      await dispatch({ type: "LOGIN", payload: res.user });
+
+      // update online:bool for currentUser doc in db to false
+      const docRef = await doc(db, `users/${res.user.uid}`);
+      updateDoc(docRef, { online: true });
 
       if (!isCancelled) {
         setIsPending(false);
         setError(null);
       }
     } catch (err) {
+      debugger;
       if (!isCancelled) {
         setError(err.message);
         setIsPending(false);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
+import { useLogin } from "../hooks/useLogin";
+
 export default function Login() {
-  return <div className="login">Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isPending, error } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <form className="auth-form" onSubmit={(e) => handleSubmit(e)}>
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {!isPending && (
+        <button className="btn" onClick={(e) => handleSubmit(e)}>
+          Log in
+        </button>
+      )}
+      {isPending && (
+        <button className="btn" disabled>
+          loading
+        </button>
+      )}
+
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
 }
